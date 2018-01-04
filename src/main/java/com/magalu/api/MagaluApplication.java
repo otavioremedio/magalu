4ece171f5021c3969b05b686bd6d196df5166cc5
otavio.remedio <otavio.remedio@GTI578699.senac.corp>
2018-01-04 12:29:13 -0200

    root

---
+++ src/main/java/com/magalu/api/MagaluApplication.java
@@ -0,0 +1,12 @@
+package com.magalu.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagaluApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagaluApplication.class, args);
+	}
+}
